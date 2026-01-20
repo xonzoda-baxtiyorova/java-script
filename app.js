7cc75513cc139a1b33616979164b91d2aa8924c2
Xonzoda Baxtiyorova <xonzodabaxtiyorova11@gmail.com>
2026-01-20 16:23:52 +0500

    Refactor user input handling and alerts
    Updated user input prompts and alerts, commented out old code, and added new variables for user name and age.

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 // Foydalanuvchidan ma'lumot olish 
 
 
-prompt('Ismingizni kiriting')
+// prompt('Ismingizni kiriting')
 // string -yozuv orqali javob berish
 
-confirm("Ro'yxatdan o'tishni xohlaysizmi?")
+// confirm("Ro'yxatdan o'tishni xohlaysizmi?")
 // boolean - ha yoki yo'q bilan javob berish
 
 // Foydalanuvchiga ma'lumot berish
 
-alert("Done!")
+// alert("Done!")
 // foydalanuvchi uchun
-console.log("This is console massage")
+// console.log("This is console massage")
 // developerlar uchun
 
 
-console.error("This is error massage")
-console.warn("This is warning massage")
-console.table({ user: "User name", age: 20 })
+// console.error("This is error massage")
+// console.warn("This is warning massage")
+// console.table({ user: "User name", age: 20 })
 
 
 
 // O'zgaruvchilar
-let userAge = 15
+// let userAge = 15
 // let , var -kalit so'z , userage - o'zgaruvchi , 15- qiymat
 // let va varni qiymatlarini o'zgartirsa bo'ladi
 
-console.log(userAge);
-userAge = 16
-console.log(userAge);
+// console.log(userAge);
+// userAge = 16
+// console.log(userAge);
 
 
-const userBirthYear = 2011
+// const userBirthYear = 2011
 // o'zgarmas qiymatlarni saqlash uchun
-userBirthYear = 2012
-console.log(userBirthYear);
+// userBirthYear = 2012
+// console.log(userBirthYear);
 
+const userName = prompt("Ismingizni kiriting:")
+const userAge2 =prompt ("Yoshingizni kiriting:")
+alert("Aziza, siz 2006-yilda tug'ilgansiz:")
 
